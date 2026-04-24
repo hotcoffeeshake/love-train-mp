7a56ab0c4868ba8431467810db22815ebfd6c4f9
Qichen Xie <qichenxie@QichendeMacBook-Air.local>
2026-04-25 01:50:05 +0800

    feat(mp): message-bubble component

---
+++ miniprogram/components/message-bubble/message-bubble.ts
@@ -0,0 +1,8 @@
+Component({
+  properties: {
+    role: { type: String, value: 'user' },
+    content: { type: String, value: '' },
+    loading: { type: Boolean, value: false },
+  },
+  data: {},
+});
